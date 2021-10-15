a7465580ce27e18a66cdd743fda53c4f8f930c0b
Jonathan Couldridge <jcouldridge@outlook.com>
2021-10-15 15:46:43 +0100

    poc has a scene now

--- poc/src/main.ts
+++ poc/src/main.ts
@@ -1,8 +1,15 @@
 import "./style.css";
 import Phaser from "phaser";
 
+class MyScene extends Phaser.Scene {
+  create() {
+    this.add.text(10, 10, "Hello World");
+  }
+}
+
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  scene: [MyScene],
 });
