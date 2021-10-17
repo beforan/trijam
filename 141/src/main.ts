6bd8b6754c62ac76a1ec3117983061b6383e330e
Jon Couldridge <jcouldridge@outlook.com>
2021-10-17 10:52:21 +0100

    player 4 point movement

--- 141/src/main.ts
+++ 141/src/main.ts
@@ -1,15 +1,16 @@
 import "./style.css";
 import Phaser from "phaser";
-
-class MyScene extends Phaser.Scene {
-  create() {
-    this.add.text(10, 10, "Hello World");
-  }
-}
+import { Game } from "./scenes/Game";
 
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: [MyScene],
+  scene: [Game],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
 });
